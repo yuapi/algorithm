332a39738047bace0451926dee6dfc53a7902412
yuapi <phk2225@gmail.com>
2025-03-20 23:12:48 +0900

    [Solved] PG:42576

---
+++ Java/PG/PG_42576.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+class Solution {
+    public String solution(String[] participant, String[] completion) {
+        Arrays.sort(participant);
+        Arrays.sort(completion);
+
+        for (int i = 0; i < participant.length-1; i++) {
+            if (participant[i].equals(completion[i])) {
+                continue;
+            }
+            return participant[i];
+        }
+        return participant[participant.length-1];
+    }
+}
