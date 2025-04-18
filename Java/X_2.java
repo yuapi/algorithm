3191f32870e70dc1ab1b6aa1a3a25b346cda88e3
yuapi <phk2225@gmail.com>
2025-04-18 15:05:30 +0900

    [Solved] X:2

---
+++ Java/X_2.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] wallet, int[] bill) {
+        int answer = 0;
+
+        while ((bill[0] > wallet[0] || bill[1] > wallet[1]) && (bill[0] > wallet[1] || bill[1] > wallet[0])) {
+            if (bill[0] > bill[1]) {
+                bill[0] = bill[0] / 2;
+            } else {
+                bill[1] = bill[1] / 2;
+            }
+            answer++;
+        }
+
+        return answer;
+    }
+}
